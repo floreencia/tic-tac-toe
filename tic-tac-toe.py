5568b2cbbb9cf89dcce1547fba1400d5a08d2c11
florencia <floreencia@gmail.com>
2026-09-11 16:12:30 +0200

    initial commit - s1 variablesand board state

---
+++ tic-tac-toe.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+print("\nWelcome to Tic-Tac-Toe!\n")
+
+top_right = 'X'
+top_middle = 'O'
+top_left = ' '
+
+middle_right = ' '
+middle_middle = 'X'
+middle_left = ' '
+
+lower_right = ' '
+lower_middle = ' '
+lower_left = 'O'
+
+#
+print(f" {top_right} | {top_middle} | {top_left}")
+print("---+---+---")
+print(f" {middle_right} | {middle_middle} | {middle_left}")
+print("---+---+---")
+print(f" {lower_right} | {lower_middle} | {lower_left}")
+
+print("\n")
